8a4bacd4485d93eccea94c9822863ab2e03ad7dc
AzimeX <ajilkhomov@gmail.com>
2018-05-19 18:52:29 -0400

    put tests in a separate file

--- src/main.cpp
+++ src/main.cpp
@@ -2,16 +2,15 @@
 #include <gtest/gtest.h>
 #include "Screen.h"
 #include "Point.h"
+#include "tests/main.h"
 
 int add(int x, int y){
     return x+y;
 }
 
-TEST(FirstTest, AdditionTest){
-    ASSERT_EQ(10, add(5,5));
-}
- 
 int main(int argc, char **argv){
+    Screen screen;
+    screen.~Screen();
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
---
+++ src/tests/main.h
@@ -0,0 +1,8 @@
+#define MAIN_H
+#ifndef MAIN_H
+
+TEST(FirstTest, AdditionTest){
+    ASSERT_EQ(10, add(5,5));
+}
+
+#endif
