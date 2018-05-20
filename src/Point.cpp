f09bc923c1988e14b9093a58bd1f7e86274c910e
AzimeX <ajilkhomov@gmail.com>
2018-05-19 20:52:17 -0400

    define point in terms of coordinates

--- src/Point.cpp
+++ src/Point.cpp
@@ -1,1 +1,61 @@
 #include "Point.h"
+
+Point::Point(){
+	cartesian.X = 0;
+	cartesian.Y = 0;
+}
+
+Point::Point(int Y, int X){
+	cartesian.Y = Y;
+	cartesian.X = X;
+	// (Y, X)
+	// (1, 1) Quadrant I
+	if( Y > 0 && X > 0) 		quadrant = I;
+	// (1, -1) Quadrant II
+	else if( Y > 0 && X < 0)	quadrant = II;
+	// (-1, -1) Quadrant III
+	else if( Y < 0 && X < 0)	quadrant = III;
+	// (-1, 1) Quadrant IV
+	else if( Y < 0 && X > 0)	quadrant = IV;
+	// (0, 0)
+	else
+		;	// Do nothing
+}
+
+/*
+	Function:	converts cartesian coordinates to absolute coordinates
+	@param:		screen, used to obtain screen size
+	@return:	void
+*/
+void Point::convert(Screen &screen){
+	absolute.Y = abs(cartesian.Y);
+	absolute.X = abs(cartesian.X);
+	switch(quadrant){
+		case I:	
+			absolute.X = absolute.X + screen.getColumns()/2;
+			break;
+		case II:
+			// All done
+			break;
+		case III:
+			absolute.Y = absolute.Y + screen.getRows()/2;
+			break;
+		case IV:
+			absolute.X = absolute.X + screen.getColumns()/2;
+			absolute.Y = absolute.Y + screen.getRows()/2;
+			break;
+		default: 
+			break;
+	}			
+
+}
+
+/*
+	Function:	Print coordinate
+	@param:		screen it is being printed on
+	@return:	void
+*/
+void Point::print(int Y, int X, Screen &screen){
+	mvprintw(Y, X, "ABS: ( %4d, %4d )", absolute.Y, absolute.X);
+	mvprintw(Y+1, X, "REL: ( %4d, %4d )", cartesian.Y, cartesian.X);
+}
--- src/Point.h
+++ src/Point.h
@@ -1,15 +1,28 @@
 #ifndef POINT_H
 #define POINT_H
 
+#include "Screen.h"
+#include <cmath>
+
+struct Coordinate{ int X; int Y; };
+enum Quadrant{ I, II, III, IV };
+
+/*
+	Function: Point class is used to represent different points in space
+		- Knows which quadrant it is in
+*/
 class Point{
 private:
-	int X;
-	int Y;
+	Coordinate cartesian;
+	Coordinate absolute;
+	Quadrant quadrant;
 public:
-	Point() : Y(0), X(0) {}
-	Point(int Y, int X) : Y(Y), X(X) {}
-	int getX() { return X; }
-	int getY() { return Y; }
+	Point();
+	Point(int Y, int X);
+	Coordinate getAbsolute() { return absolute; }
+	Coordinate getCartesian() { return cartesian; }
+	void convert(Screen &screen);
+	void print(int Y, int X, Screen &screen);
 };
 
 #endif
